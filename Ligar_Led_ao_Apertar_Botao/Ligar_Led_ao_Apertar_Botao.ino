710e0e804ec38c434df216426419cd085ea9f0a3
jairogp <jairogp@users.noreply.github.com>
2017-05-07 04:37:26 -0300

    Add files via upload

---
+++ Ligar_Led_ao_Apertar_Botao/Ligar_Led_ao_Apertar_Botao.ino
@@ -0,0 +1,11 @@
+
+ void setup() {
+   pinMode (5, OUTPUT);
+   pinMode (3, INPUT);
+   }
+
+ void loop() {
+   int estado;
+   estado = digitalRead (3);
+   digitalWrite (5, estado);
+ }
---
+++ Ligar_Led_ao_Apertar_Botao_com_Pull_Up/Ligar_Led_ao_Apertar_Botao_com_Pull_Up.ino
@@ -0,0 +1,24 @@
+/*
+ void setup() {
+   pinMode (5, OUTPUT);
+   pinMode (3, INPUT_PULLUP);
+   }
+
+ void loop() {
+   int estado;
+   estado = digitalRead (3);
+   digitalWrite (5, !estado);
+ }
+ */
+ 
+
+ void setup() {
+  pinMode(5, OUTPUT);
+  pinMode(3, INPUT_PULLUP);
+ }
+ 
+ void loop() {
+  digitalWrite(5, !digitalRead (3) );
+}
+ 
+  
